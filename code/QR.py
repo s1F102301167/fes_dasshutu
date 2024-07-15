9fd11b83f4c59c2112c67ce77192e5bb7203dd90
s1F102301167 <136313317+s1F102301167@users.noreply.github.com>
2024-07-16 01:35:02 +0900

    htmlとQRの作成

---
+++ code/QR.py
@@ -0,0 +1,18 @@
+import qrcode
+
+# URLを指定
+url = "https://example.com/my_page.html"
+
+# QRコードを生成
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# 画像として保存
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("my_qr_code.png")
